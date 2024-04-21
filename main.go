09c824a12e06343ee0c3fa074e9468273e69e9b2
Booklynn <95514331+Booklynn@users.noreply.github.com>
2024-04-21 17:40:19 +0700

    feat: Add basic auth and graceful shutdown

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 )
 
@@ -14,10 +19,9 @@ func main() {
 	connectDb()
 
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
-	})
-	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
+	e.Use(addBasicAuthMiddleware())
+	registerRoutes(e)
+	startServer(e)
 }
 
 func connectDb() {
@@ -28,4 +32,53 @@ func connectDb() {
 	}
 
 	defer conn.Close()
+}
+
+func addBasicAuthMiddleware() echo.MiddlewareFunc {
+	authFn := func(username, password string, ctx echo.Context) (bool, error) {
+		adminUser, adminPassword := prepareAdminUserPas(os.Getenv("ADMIN_USERNAME"), os.Getenv("ADMIN_PASSWORD"))
+
+		return username == adminUser && password == adminPassword, nil
+	}
+
+	return middleware.BasicAuth(authFn)
+}
+
+func prepareAdminUserPas(username, password string) (adminUsername, adminPassword string) {
+	if username == "" && password == "" {
+		return "adminTax", "admin!"
+	}
+
+	return username, password
+}
+
+func registerRoutes(e *echo.Echo) {
+	e.GET("/", handleRoot)
+}
+
+func handleRoot(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, Go Bootcamp!")
+}
+
+func startServer(e *echo.Echo) {
+	go func() {
+		if err := e.Start(":" + os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %v", err)
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	<-shutdown
+	log.Printf("Received shutdown signal")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		log.Fatalf("Shutdonw failed: %v", err)
+	}
+
+	log.Println("Shutdown completed")
 }
