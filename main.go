f56946be7eded9d2ff907b6a1fe5ebe928b7f044
Booklynn <95514331+Booklynn@users.noreply.github.com>
2024-04-21 14:56:29 +0700

    feat: Add connectDb function and change PORT hard code to env variable

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	connectDb()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
+}
+
+func connectDb() {
+	conn, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		log.Fatal("Cannot connect to database.", err)
+	}
+
+	defer conn.Close()
 }
